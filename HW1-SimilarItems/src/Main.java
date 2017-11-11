47efb013f57393eb87ae9ed8c72c3367ec069c67
YantianYou <yantian@kth.se>
2017-11-11 11:59:19 +0100

    Test function

--- HW1-SimilarItems/src/Main.java
+++ HW1-SimilarItems/src/Main.java
@@ -5,21 +5,31 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.io.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// Shingling test
+		String route="/dataset/";
+		// Shingling test		
 		Shingling s = new Shingling();
-		String text1 = "abdabc";
-		String text2 = "abd ch eg s dc";
-		String text3 = "abd ch eg s gf";
-		String text4 = "";
+		//String text1 = "abdabc";
+		//String text2 = "abd ch eg s dc";
+		//String text3 = "abd ch eg s gf";
+		//String text4 = "";
+		String text1 = readFile(route+"bathroom_bestwestern_hotel_sfo.1.gold");
+		String text2 = readFile(route+"bathroom_bestwestern_hotel_sfo.2.gold");
+		String text3 = readFile(route+"location_holiday_inn_london.1.gold");
+		String text4 = readFile(route+"location_holiday_inn_london.2.gold");
+		String text5 = readFile(route+"comfort_honda_accord_2008.1.gold");
+		String text6 = readFile(route+"comfort_honda_accord_2008.2.gold");
 		
-		HashMap<Integer,String> shingle1 = s.ShingleHash(text1, 3);
-		HashMap<Integer,String> shingle2 = s.ShingleHash(text2, 3);
-		HashMap<Integer,String> shingle3 = s.ShingleHash(text3, 3);
-		HashMap<Integer,String> shingle4 = s.ShingleHash(text4, 3);
+		HashMap<Integer,String> shingle1 = s.ShingleHash(text1, 4);
+		HashMap<Integer,String> shingle2 = s.ShingleHash(text2, 4);
+		HashMap<Integer,String> shingle3 = s.ShingleHash(text3, 4);
+		HashMap<Integer,String> shingle4 = s.ShingleHash(text4, 4);
+		HashMap<Integer,String> shingle5 = s.ShingleHash(text5, 4);
+		HashMap<Integer,String> shingle6 = s.ShingleHash(text6, 4);
 		
 		System.out.println("--- Shingle hash values of text 1 ---");
 		System.out.println(shingle1);
@@ -29,30 +39,31 @@ public class Main {
 		System.out.println(shingle3);
 		System.out.println("--- Shingle hash values of text 4 ---");
 		System.out.println(shingle4); 
+		System.out.println("--- Shingle hash values of text 5 ---");
+		System.out.println(shingle5); 
+		System.out.println("--- Shingle hash values of text 6 ---");
+		System.out.println(shingle6); 
 		
 		// CompareSets test
 		Set<Integer> set1 = shingle1.keySet();
 		Set<Integer> set2 = shingle2.keySet();
 		Set<Integer> set3 = shingle3.keySet();
 		Set<Integer> set4 = shingle4.keySet();
-		Set<Integer> set5 = shingle4.keySet();
-		/*int[] a1 = {1, 3, 5, 7}; int[] a2 = {1, 3, 4, 9}; int[] a3 = {0, 5}; int[] a4 = {}; int[] a5 = {};
-		set1 = Arrays.stream(a1).boxed().collect(Collectors.toSet());
-		set2 = Arrays.stream(a2).boxed().collect(Collectors.toSet());
-		set3 = Arrays.stream(a3).boxed().collect(Collectors.toSet());
-		set4 = Arrays.stream(a4).boxed().collect(Collectors.toSet());
-		set5 = Arrays.stream(a5).boxed().collect(Collectors.toSet());*/
+		Set<Integer> set5 = shingle5.keySet();
+		Set<Integer> set6 = shingle6.keySet();
 
 		System.out.println("--- CompareSets test 1 ---");
 		System.out.println(new CompareSets().ComputeJaccard(set1, set2));
 		System.out.println("--- CompareSets test 2 ---");
-		System.out.println(new CompareSets().ComputeJaccard(set2, set3));
+		System.out.println(new CompareSets().ComputeJaccard(set1, set3));
 		System.out.println("--- CompareSets test 3 ---");
 		System.out.println(new CompareSets().ComputeJaccard(set1, set4));
 		System.out.println("--- CompareSets test 4 ---");
-		System.out.println(new CompareSets().ComputeJaccard(set4, set5));
+		System.out.println(new CompareSets().ComputeJaccard(set1, set5));
+		System.out.println("--- CompareSets test 5 ---");
+		System.out.println(new CompareSets().ComputeJaccard(set1, set6));
 		
-		HashMap<Integer,text> Matrix=new MinHashing(set1,set2).minhashFunction(10);
+		HashMap<Integer,text> Matrix=new MinHashing(set1,set2).minhashFunction(50);
 		System.out.println("--- Signature test 1 ---");
 		System.out.println("Signature for text1"+Matrix.get(1).signatures );
 		System.out.println("Signature for text2"+Matrix.get(2).signatures );
@@ -61,15 +72,65 @@ public class Main {
 						+new CompareSignatures().similar(Matrix.get(1).signatures, Matrix.get(2).signatures) );
 		
 
-		Matrix=new MinHashing(set2,set3).minhashFunction(10);
-		System.out.println("--- Signature test 1 ---");
+		Matrix=new MinHashing(set1,set3).minhashFunction(50);
+		System.out.println("--- Signature test 2 ---");
 		System.out.println("Signature for text1"+Matrix.get(1).signatures );
-		System.out.println("Signature for text2"+Matrix.get(2).signatures );
+		System.out.println("Signature for text3"+Matrix.get(2).signatures );
 		
-		System.out.println("Similarity of signature for text2 and text3: "
+		System.out.println("Similarity of signature for text1 and text3: "
+						+new CompareSignatures().similar(Matrix.get(1).signatures, Matrix.get(2).signatures) );
+		
+		Matrix=new MinHashing(set1,set4).minhashFunction(50);
+		System.out.println("--- Signature test 3 ---");
+		System.out.println("Signature for text1"+Matrix.get(1).signatures );
+		System.out.println("Signature for text4"+Matrix.get(2).signatures );
+		
+		System.out.println("Similarity of signature for text1 and text4: "
+						+new CompareSignatures().similar(Matrix.get(1).signatures, Matrix.get(2).signatures) );
+	
+		Matrix=new MinHashing(set1,set5).minhashFunction(50);
+		System.out.println("--- Signature test 4 ---");
+		System.out.println("Signature for text1"+Matrix.get(1).signatures );
+		System.out.println("Signature for text5"+Matrix.get(2).signatures );
+		
+		System.out.println("Similarity of signature for text1 and text5: "
+						+new CompareSignatures().similar(Matrix.get(1).signatures, Matrix.get(2).signatures) );
+	
+		Matrix=new MinHashing(set1,set6).minhashFunction(50);
+		System.out.println("--- Signature test 5 ---");
+		System.out.println("Signature for text1"+Matrix.get(1).signatures );
+		System.out.println("Signature for text6"+Matrix.get(2).signatures );
+		
+		System.out.println("Similarity of signature for text1 and text6: "
 						+new CompareSignatures().similar(Matrix.get(1).signatures, Matrix.get(2).signatures) );
 	
 
 	}
+	public static String readFile(String fileName) {
+        File file = new File(fileName);
+        BufferedReader reader = null;
+        String FullString="";
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String tempString = null;
+            int line = 1;
+            while ((tempString = reader.readLine()) != null) {
+            	FullString+=tempString;
+                line++;
+            }
+            reader.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                }
+            }
+        }
+        return FullString;
+    }
 
 }
