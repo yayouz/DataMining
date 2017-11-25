f1d638547f3ddead8eb616a3ca7dcaafb9ffea9c
txzhao <tzh@kth.se>
2017-11-25 23:59:23 +0100

    save data in a csv file for further plots and evaluations

--- HW3-StreamingData/streamingData/Main.java
+++ HW3-StreamingData/streamingData/Main.java
@@ -1,7 +1,11 @@
 package streamingData;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,6 +15,7 @@ public class Main {
 		FileInputStream fis = new FileInputStream(path);
 		int time=0;
 		int M=5000;
+		ArrayList<Integer> allCounts = new ArrayList<Integer>();
 		Triest triest=new Triest(time,M);
         Scanner scanner = new Scanner(fis);
         scanner.useDelimiter("\\n");
@@ -23,11 +28,23 @@ public class Main {
            }
             Edge edge=new Edge(node.get(0),node.get(1));
             triest.streaming(edge);
+            allCounts.add(triest.getGlobalCount());
             System.out.println("global count: "+triest.getGlobalCount());
             //System.out.println("local count: "+triest.getLocalCount());
             lineInt.close();
         }
         
         scanner.close();
+        
+        // save global count in a csv file for plots and evaluation
+        File csvFile = new File("/Users/txzhao/Desktop/counts.csv");
+        try (PrintWriter csvWriter = new PrintWriter(new FileWriter(csvFile));){
+          for(int count : allCounts){
+            csvWriter.println(count);
+          }
+        } catch (IOException e) {
+            //Handle exception
+            e.printStackTrace();
+        }
 	}
 }
